d0ec860c3be384c44b102bbb6ec5d3044c916171
James Spencer <jameskcs1254@gmail.com>
2022-01-04 16:58:00 +0000

    Refactor to express

--- index.js
+++ index.js
@@ -6,11 +6,25 @@
 */
 
 const express = require('express');
+const path = require('path')
+
 const app = express();
 const port = 8080;
 
 app.get('/', function(req, res) {
-  res.send()
+  res.sendFile(path.join(__dirname, '/index.html'))
+});
+
+app.get('/about', function(req, res) {
+  res.sendFile(path.join(__dirname, '/about.html'))
+});
+
+app.get('/contact-me', function(req, res) {
+  res.sendFile(path.join(__dirname, '/contact-me.html'))
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '/404.html'))
 });
 
 app.listen(port, function() {
